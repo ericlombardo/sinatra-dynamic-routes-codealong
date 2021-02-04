26386a0064986d753628c3672d90eb99e53d58a7
Eric <eric.m.lombardo@gmail.com>
2021-02-04 14:21:35 -0500

    commit tree d70b06da906b521995b03a6e7e94ba145b714d94

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -13,6 +14,20 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  # Match route, assign what's after goodbye/ to hash with :name as key
+  # assign value of :name to @name, send to .erb file to format and respond to client
+  get '/goodbye/:name' do   
+    @name = params[:name]
+    erb :goodbye
+  end
+
+  # Match route, assign 2 params to instance variables, convert to integers
+  # send to multiply.erb to format display and respond
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+
+    erb :multiply
+  end
 
 end
